a14d7e6c81f613ccbfc925f720b09b2891ec5300
smyha <smyhail@gmail.com>
2020-01-31 20:56:23 +0100

    test

--- src/main/java/com/sub/Main.java
+++ src/main/java/com/sub/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
     @Override    public void start(Stage primaryStage) throws Exception{
         this.stage=primaryStage;
         mainWindow();
+        System.out.println("done!!!");
     }
     public void mainWindow() {
         try {
